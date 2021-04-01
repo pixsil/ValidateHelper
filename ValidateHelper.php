034eddd176c69fbba9b35b9ac70cf6b3cec1df2d
pixsil <57232302+pixsil@users.noreply.github.com>
2021-04-01 10:41:31 +0200

    Created new file

---
+++ ValidateHelper.php
@@ -0,0 +1,114 @@
+<?php
+
+// version 10
+
+if (!function_exists('validate')) {
+    function validate($type, $other_validation = [])
+    {
+        // init (works for array or string)
+        $validation_arr = is_string($other_validation) ? explode('|', $other_validation) : $other_validation;
+
+        // guard: present must not check other stuff
+        if (in_array('present', $validation_arr)) {
+            return ['present'];
+        }
+
+        // set keys to no override
+        foreach ($validation_arr as $key => $validation) {
+
+            // explode for key
+            $validation_explode = explode(':', $validation);
+
+            // set key
+            $validation_arr[$validation_explode[0]] = $validation;
+
+            // remove old value
+            unset($validation_arr[$key]);
+        }
+
+        // sometimes of required
+        if (in_array('nullable', $validation_arr)) {
+            $extra_arr = ['nullable' => 'nullable'];
+        } elseif (in_array('sometimes', $validation_arr)) {
+            $extra_arr = ['sometimes' => 'sometimes'];
+        } else {
+            $extra_arr = ['required' => 'required'];
+        }
+
+        // add
+        switch ($type) {
+
+            // input
+            case 'input':
+            case 'varchar':
+            case 'string':
+                $extra_arr['string'] = 'string';
+                $extra_arr['max'] = 'max:255';
+                break;
+
+
+            // password
+            case 'password':
+                $extra_arr['string'] = 'string';
+                $extra_arr['min'] = 'min:8';
+                $extra_arr['max'] = 'max:255';
+                $extra_arr['confirmed'] = 'confirmed';
+                break;
+
+            // textarea
+            case 'text':
+            case 'textarea':
+                $extra_arr['max'] = 'max:500';
+                break;
+
+            // checkbox or array
+            case 'array':
+            case 'checkbox':
+                $extra_arr['array'] = 'array';
+                break;
+
+            // textarea
+            case 'integer':
+            case 'number':
+            case 'int':
+                $extra_arr['integer'] = 'integer';
+                $extra_arr['max'] = 'max:2147483647';
+                break;
+
+            // boolean
+            case 'boolean':
+            case 'bool':
+                $extra_arr['boolean'] = 'boolean';
+                break;
+
+            // email
+            case 'email':
+                $extra_arr['email'] = 'email';
+                $extra_arr['max'] = 'max:255';
+                break;
+
+            // email
+            case 'image':
+                // size in kb
+                $extra_arr['image'] = 'image';
+                $extra_arr['max'] = 'max:5000';
+                break;
+
+            // file
+            case 'file':
+            case 'upload':
+                // size in kb
+                $extra_arr['file'] = 'file';
+                $extra_arr['max'] = 'max:5000';
+                break;
+
+            default:
+                break;
+        }
+
+        // merge with already given array
+        $validation_arr = array_merge($extra_arr, $validation_arr);
+
+        return $validation_arr;
+    }
+}
